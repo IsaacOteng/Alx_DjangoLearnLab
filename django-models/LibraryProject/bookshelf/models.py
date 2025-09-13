481a80454205eb7e5f666781a466c9fabfee21be
Isaac Oteng <IsaacOteng@github.com>
2025-09-13 20:27:49 +0000

    Implementing Advanced Model Relationships in Django

---
+++ django-models/LibraryProject/bookshelf/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)         # Book title
+    author = models.CharField(max_length=100)        # Author's name
+    publication_year = models.IntegerField()         # Year published
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,17 @@
+from relationship_app.models import Author
+from relationship_app.models import Book
+from relationship_app.models import Library
+from relationship_app.models import Librarian
+
+
+author = Author.objects.get(name="George Orwell")
+books_by_author = Book.objects.filter(author=author)
+
+library = Library.objects.get(name="Central Library")
+all_books = library.books.all()
+
+librarian = Librarian.objects.get(name="Alice")
+library_of_librarian = librarian.library
+
+
+
