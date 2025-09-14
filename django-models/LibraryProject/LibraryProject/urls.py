667b8531be4e35ce304dfc10e388aff625d1d38f
Isaac Oteng <IsaacOteng@github.com>
2025-09-14 22:27:44 +0000

    URL Config

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('books/', views.book_list, name='Book list'),
+    path('library/', views.LibraryDetailView.as_view(), name='Library Detail'),
 ]
