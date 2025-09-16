2f45d028027975207114197748c3d23f94cdb10c
Isaac Oteng <IsaacOteng@github.com>
2025-09-16 17:25:43 +0000

    Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,7 @@ from .models import Book
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth import login
 
 # Create your views here.
 def list_books(request):
